ffdea8ef59eb30ff2581c3414d6dff3a365f5417
SsAaFfIiKk <chf700@yandex.ru>
2021-09-01 23:01:11 +0300

    first comit

---
+++ server.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://127.1.0.0.1",
+    "http://127.1.0.0.1:8000",
+]
+
+templates = Jinja2Templates(directory=".")
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", response_class=HTMLResponse)
+def homepage(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/get_logo")
+def logo():
+    return  FileResponse("logo.jpg")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
